595f963a05b4512bee6fc32f4ae668f400e0a1f0
leandro992 <victorleandro100@outlook.com>
2019-06-25 02:32:12 -0300

    Criação dos arquivos footer, maps, menu/ dependencia para o google maps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './src/menu';
+import footer from './src/footer';
+import maps from './src/maps'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
+       
+
+
       </header>
     </div>
   );
 }
+
 
 export default App;
